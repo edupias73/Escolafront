0d5b7bf2c6381c097772bde98adcc7e82ee4e66b
edupias73 <eduardpias0073@gmail.com>
2025-03-30 20:26:43 -0300

    telas e preparacao para as modals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,29 @@ import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { AlunoListComponent } from './components/aluno/aluno-list/aluno-list.component';
 import { AlunoFormComponent } from './components/aluno/aluno-form/aluno-form.component';
+import { CursoListComponent } from './components/curso/curso-list/curso-list.component';
+import { CursoFormComponent } from './components/curso/curso-form/curso-form.component';
+import { ProfessorListComponent } from './components/professor/professor-list/professor-list.component';
+import { ProfessorFormComponent } from './components/professor/professor-form/professor-form.component';
+import { TurmaListComponent } from './components/turma/turma-list/turma-list.component';
+import { TurmaFormComponent } from './components/turma/turma-form/turma-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginlistComponent },
   { path: 'admin', component: PrincipalComponent, children:[
-    {path: 'alunos', component: AlunoListComponent}
-   
-   
+    {path: 'alunos', component: AlunoListComponent},
+    {path: 'alunos/new',component:AlunoFormComponent},
+    {path: 'alunos/new/:id',component:AlunoFormComponent},   
+    {path: 'curso', component: CursoListComponent},
+    {path: 'curso/new',component:CursoFormComponent},
+    {path: 'curso/new/:id',component:CursoFormComponent},   
+    {path: 'professor', component: ProfessorListComponent},
+    {path: 'professor/new',component:ProfessorFormComponent},
+    {path: 'professor/new/:id',component:ProfessorFormComponent},  
+    {path: 'turma', component: TurmaListComponent},
+    {path: 'turma/new',component:TurmaFormComponent},
+    {path: 'turma/new/:id',component:TurmaFormComponent}
   ]}
   
 ];
--- src/app/components/layout/login/login-list/loginlist.component.ts
+++ src/app/components/layout/login/login-list/loginlist.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-loginlist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loginlist.component.html',
   styleUrl: './loginlist.component.scss'
 })
 export class LoginlistComponent {
+
+    Usuario!: string;
+    Senha!: string;
+
+  router = inject(Router);
+
+    logar(){
+      if(this.Usuario == "admin" && this.Senha == "admin"){
+        this.router.navigate(['admin/alunos'])
+      }else
+      alert('usuario ou senha invalidos!');
+    }
 
 }
